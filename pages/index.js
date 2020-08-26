f9e9ce97fb3e17190fb9e687d724cc3e5fe2f41a
Muhammad Fakhri <muhammad_fakhri@apps.ipb.ac.id>
2020-08-26 19:22:52 +0700

    Remove hotspot map and change it with patroli map at front page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import classNames from "classnames";
 import { makeStyles } from "@material-ui/core/styles";
-import Icon from '@material-ui/core/Icon';
+import Datetime from "react-datetime";
+import FormControl from "@material-ui/core/FormControl";
 import SiteLayout from '../components/Layout/SiteLayout';
 import Parallax from "../components/Parallax/Parallax.js";
 import GridContainer from "../components/Grid/GridContainer.js";
+import MapContainer from '../components/Map/MapPatroli';
 import GridItem from "../components/Grid/GridItem.js";
-import Map from "../components/Map/MapHotspot";
 import fetch from 'isomorphic-unfetch';
 import styles from "../assets/jss/nextjs-material-kit/pages/frontPage.js";
 const useStyles = makeStyles(styles);
@@ -35,8 +36,63 @@ function getTodayDate() {
   }
 }
 
-function FrontPage(props) {
+export default function FrontPage(props) {
   const classes = useStyles();
+  const [date, setDate] = React.useState(getTodayDate().plainDate);
+  const [mandiri, setMandiri] = React.useState(0);
+  const [pencegahan, setPencegahan] = React.useState(0);
+  const [terpadu, setTerpadu] = React.useState(0);
+  const [spots, setSpots] = React.useState();
+
+  const getPatroli = async (date) => {
+    try {
+      let patroliSpots = new Array();
+      let counter = {
+        mandiri: 0,
+        pencegahan: 0,
+        terpadu: 0
+      };
+      const url = `http://103.129.223.216/api/simadu/list?tanggal_patroli=${date}`;
+      const res = await (await fetch(url)).json();
+      let patroliData = res.data;
+      patroliData.forEach((item) => {
+        item.forEach(patroli => {
+          let data = {};
+          data.latitude = patroli.laporanDarat[0].latitude;
+          data.longitude = patroli.laporanDarat[0].longitude;
+
+          const baseMarkerUrl = 'http://maps.google.com/mapfiles/ms/icons/';
+          if (patroli.kategori_patroli == 'Mandiri') {
+            data.marker = baseMarkerUrl + "blue-dot.png";
+            counter.mandiri++;
+          }
+          if (patroli.kategori_patroli == 'Pencegahan') {
+            data.marker = baseMarkerUrl + "green-dot.png";
+            counter.pencegahan++;
+          }
+          if (patroli.kategori_patroli == 'Terpadu') {
+            data.marker = baseMarkerUrl + "yellow-dot.png";
+            counter.terpadu++;
+          }
+          data.patroli = patroli;
+
+          patroliSpots.push(data);
+        });
+      });
+
+      setSpots(patroliSpots);
+      setMandiri(counter.mandiri);
+      setPencegahan(counter.pencegahan);
+      setTerpadu(counter.terpadu);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  React.useEffect(() => {
+    getPatroli(getTodayDate().plainDate);
+  }, []);
+
   return (
     <SiteLayout scrollChange={true}>
       <div>
@@ -55,39 +111,46 @@ function FrontPage(props) {
           </div>
         </Parallax>
         <div className={classNames(classes.main, classes.mainRaised, classes.textCenter)}>
-          <h2>
-            <Icon className={classes.icon} color={"error"}>fiber_manual_record</Icon>
-          SIPONGI Live Update
-          </h2>
-          <Map
-            center={
-              {
-                lat: -1.5,
-                lng: 117.384
-              }
-            }
+          <h2>Sebaran Patroli</h2>
+          <MapContainer
+            center={{
+              lat: -1.5,
+              lng: 117.384
+            }}
             zoom={5.1}
-            hotspots={props.hotspots}
+            spots={spots}
+            isLoggedin={false}
           />
           <GridContainer>
             <GridItem xs={12}>
               <h3>
-                Tanggal: {props.todayDate.date}
+                Tanggal: {date}
                 <br />
-                Pukul: {props.todayDate.time}
+                <FormControl className={classNames(classes.formChooseDate, classes.textCenter)}>
+                  <Datetime
+                    timeFormat={false}
+                    inputProps={{ placeholder: "Pilih tanggal patroli ..." }}
+                    onChange={(date) => {
+                      getPatroli(date.format('YYYY-MM-DD'));
+                      setDate(date.format('D MMMM YYYY'));
+                    }}
+                    closeOnSelect={true}
+                    locale="id"
+                  />
+                </FormControl>
               </h3>
             </GridItem>
             <GridItem xs={4}>
-              <h2>Titik Panas</h2>
-              <h3>{props.hotspots.length}</h3>
+              <h2 className={classes.terpaduBg}>Patroli Terpadu</h2>
+              <h3>{terpadu}</h3>
             </GridItem>
             <GridItem xs={4}>
-              <h2>Rentang Data</h2>
-              <h3>24h</h3>
+              <h2 className={classes.mandiriBg}>Patroli Mandiri</h2>
+              <h3>{mandiri}</h3>
             </GridItem>
             <GridItem xs={4}>
-              <h2>Confidence Level</h2>
-              <h3>80%</h3>
+              <h2 className={classes.pencegahanBg}>Patroli Pencegahan</h2>
+              <h3>{pencegahan}</h3>
             </GridItem>
           </GridContainer>
         </div>
@@ -95,32 +158,3 @@ function FrontPage(props) {
     </SiteLayout>
   );
 }
-
-export async function getStaticProps() {
-  let todayDate = getTodayDate();
-  let province = 'a';
-  let hotspotResponses = new Array();
-  let hotspots = new Array();
-
-  try {
-    const url = `http://103.129.223.216/siavipala/public/api/hotspot-sipongi/date-range?start_date=${todayDate.plainDate}&end_date=${todayDate.plainDate}&provinsi=${province}`;
-    const res = await (await fetch(url)).json();
-    hotspotResponses = res.hostspot_sipongi;
-    hotspotResponses.forEach((item) => {
-      item.sebaran_hotspot.forEach((item) => {
-        hotspots.push(item);
-      });
-    });
-  } catch (error) {
-    console.log(error);
-  }
-
-  return {
-    props: {
-      hotspots,
-      todayDate,
-    }
-  }
-}
-
-export default FrontPage
