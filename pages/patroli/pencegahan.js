3e092cd4ef97ac28597c9511c4809bd8c31e4d5b
Muhammad Fakhri <muhammad_fakhri@apps.ipb.ac.id>
2020-09-03 14:18:29 +0700

    Apply data fetching in patroli pages

--- pages/patroli/pencegahan.js
+++ pages/patroli/pencegahan.js
@@ -1,119 +1,28 @@
-import dynamic from "next/dynamic";
+import { Paper } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import CloudDownloadIcon from '@material-ui/icons/CloudDownload';
+import classNames from 'classnames';
+import MaterialTable from 'material-table';
+import dynamic from 'next/dynamic';
 import Router from 'next/router';
-import { getTokenFromRequest } from '../../context/auth';
-const LoginPage = dynamic(() => import("../login"));
-import PropTypes from 'prop-types';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TablePagination from '@material-ui/core/TablePagination';
-import TableRow from '@material-ui/core/TableRow';
-import TableFooter from '@material-ui/core/TableFooter';
-import Paper from '@material-ui/core/Paper';
-import IconButton from '@material-ui/core/IconButton';
-import FirstPageIcon from '@material-ui/icons/FirstPage';
-import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
-import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
-import LastPageIcon from '@material-ui/icons/LastPage';
+import useSWR from 'swr';
+const LoginPage = dynamic(() => import('../login'));
+import styles from '../../assets/jss/nextjs-material-kit/pages/patroliPencegahanPage';
 import SiteLayout from '../../components/Layout/SiteLayout';
-import classNames from "classnames";
-import styles from "assets/jss/nextjs-material-kit/pages/patroliPencegahanPage";
+import { getTokenFromRequest } from '../../context/auth';
+import { simaduApiUrl } from '../../services/config';
+import PatroliService from '../../services/PatroliService';
 
-const useStyles1 = makeStyles((theme) => ({
-    root: {
-        flexShrink: 0,
-        marginLeft: theme.spacing(2.5),
-    },
-}));
-
-function TablePaginationActions(props) {
-    const classes = useStyles1();
-    const theme = useTheme();
-    const { count, page, rowsPerPage, onChangePage } = props;
-
-    const handleFirstPageButtonClick = (event) => {
-        onChangePage(event, 0);
-    };
-
-    const handleBackButtonClick = (event) => {
-        onChangePage(event, page - 1);
-    };
-
-    const handleNextButtonClick = (event) => {
-        onChangePage(event, page + 1);
-    };
-
-    const handleLastPageButtonClick = (event) => {
-        onChangePage(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
-    };
-
-    return (
-        <div className={classes.root}>
-            <IconButton
-                onClick={handleFirstPageButtonClick}
-                disabled={page === 0}
-                aria-label="first page"
-            >
-                {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
-            </IconButton>
-            <IconButton onClick={handleBackButtonClick} disabled={page === 0} aria-label="previous page">
-                {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
-            </IconButton>
-            <IconButton
-                onClick={handleNextButtonClick}
-                disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-                aria-label="next page"
-            >
-                {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
-            </IconButton>
-            <IconButton
-                onClick={handleLastPageButtonClick}
-                disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-                aria-label="last page"
-            >
-                {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
-            </IconButton>
-        </div>
-    );
-}
-
-TablePaginationActions.propTypes = {
-    count: PropTypes.number.isRequired,
-    onChangePage: PropTypes.func.isRequired,
-    page: PropTypes.number.isRequired,
-    rowsPerPage: PropTypes.number.isRequired,
-};
-
-function createData(name, calories, fat) {
-    return { name, calories, fat };
-}
-
-const rows = [
-    createData('Cupcake', 305, 3.7),
-    createData('Donut', 452, 25.0),
-    createData('Eclair', 262, 16.0),
-    createData('Frozen yoghurt', 159, 6.0),
-    createData('Gingerbread', 356, 16.0),
-    createData('Honeycomb', 408, 3.2),
-    createData('Ice cream sandwich', 237, 9.0),
-    createData('Jelly Bean', 375, 0.0),
-    createData('KitKat', 518, 26.0),
-    createData('Lollipop', 392, 0.2),
-    createData('Marshmallow', 318, 0),
-    createData('Nougat', 360, 19.0),
-    createData('Oreo', 437, 18.0),
-].sort((a, b) => (a.calories < b.calories ? -1 : 1));
+const column = [
+    { title: 'Tanggal', field: 'patrolDate' },
+    { title: 'Daerah Operasi', field: 'operationRegion' },
+    { title: 'Daerah Patroli', field: 'patrolRegion' },
+];
 
 function PatroliPencegahanPage(props) {
     const useStyles = makeStyles(styles);
     const classes = useStyles();
-    const [page, setPage] = React.useState(0);
-    const [rowsPerPage, setRowsPerPage] = React.useState(5);
-
-    const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
+    const [pencegahan, setPencegahan] = React.useState();
 
     // Load login page if not logged in
     React.useEffect(() => {
@@ -125,83 +34,36 @@ function PatroliPencegahanPage(props) {
         if (!props.loggedIn) return <LoginPage />;
     }
 
-    const handleChangePage = (event, newPage) => {
-        setPage(newPage);
-    };
-
-    const handleChangeRowsPerPage = (event) => {
-        setRowsPerPage(parseInt(event.target.value, 10));
-        setPage(0);
-    };
+    const { data, error } = useSWR(simaduApiUrl + '/list', PatroliService.getAllPatroliPencegahan);
+    // TODO: change endpoint with endpoint for fetching all pencegahan patrol data
+    React.useEffect(() => {
+        setPencegahan(data);
+    }, [data]);
 
     return (
         <SiteLayout headerColor="info">
             <div>
                 <div className={classNames(classes.main, classes.mainRaised, classes.textCenter)}>
                     <h2>Data Patroli Pencegahan</h2>
-                    <TableContainer component={Paper}>
-                        <Table className={classes.table} aria-label="custom pagination table">
-                            <TableHead>
-                                <TableRow>
-                                    <TableCell>Tim</TableCell>
-                                    <TableCell align="center">Wilayah</TableCell>
-                                    <TableCell align="center">Laporan</TableCell>
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                                {rows.length > 0
-                                    ? (rowsPerPage > 0
-                                        ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                                        : rows
-                                    ).map((row) => (
-                                        <TableRow key={row.name} hover>
-                                            <TableCell component="th" scope="row">
-                                                {row.name}
-                                            </TableCell>
-                                            <TableCell style={{ width: 300 }} align="center">
-                                                {row.calories}
-                                            </TableCell>
-                                            <TableCell style={{ width: 300 }} align="center">
-                                                {row.fat}
-                                            </TableCell>
-                                        </TableRow>
-                                    ))
-                                    : (
-                                        <TableRow style={{ height: 53 * emptyRows }}>
-                                            <TableCell colSpan={6} className={classes.textCenter}>Tidak ada data</TableCell>
-                                        </TableRow>
-                                    )
-                                }
-
-                                {rows.length > 0
-                                    ? (emptyRows > 0 && (
-                                        <TableRow style={{ height: 53 * emptyRows }}>
-                                            <TableCell colSpan={6} />
-                                        </TableRow>
-                                    ))
-                                    : null
-                                }
-                            </TableBody>
-                            <TableFooter>
-                                <TableRow>
-                                    <TablePagination
-                                        rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
-                                        colSpan={3}
-                                        count={rows.length}
-                                        rowsPerPage={rowsPerPage}
-                                        page={page}
-                                        SelectProps={{
-                                            inputProps: { 'aria-label': 'rows per page' },
-                                            native: true,
-                                        }}
-                                        onChangePage={handleChangePage}
-                                        onChangeRowsPerPage={handleChangeRowsPerPage}
-                                        ActionsComponent={TablePaginationActions}
-                                    />
-                                </TableRow>
-                            </TableFooter>
-                        </Table>
-                    </TableContainer>
+                    <MaterialTable
+                        title=''
+                        columns={column}
+                        components={{
+                            Container: props => <Paper {...props} elevation={0} />
+                        }}
+                        data={pencegahan}
+                        options={{
+                            search: true,
+                            actionsColumnIndex: -1
+                        }}
+                        actions={[
+                            {
+                                icon: CloudDownloadIcon,
+                                tooltip: 'Download Laporan',
+                                onClick: (event, rowData) => window.open(rowData.reportLink)
+                            }
+                        ]}
+                    />
                 </div>
             </div>
         </SiteLayout>
