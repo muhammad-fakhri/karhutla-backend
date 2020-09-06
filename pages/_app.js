82af0aa9443a35dc2487ee6f9ca7692ce9749526
Muhammad Fakhri <muhammad_fakhri@apps.ipb.ac.id>
2020-09-06 13:09:57 +0700

    Delete page change component

--- pages/_app.js
+++ pages/_app.js
@@ -16,33 +16,11 @@
 
 */
 import React from "react";
-import ReactDOM from "react-dom";
 import App from "next/app";
 import Head from "next/head";
-import Router from "next/router";
 import { AuthProvider } from '../context/auth';
 
-import PageChange from "components/PageChange/PageChange.js";
-
 import "assets/scss/nextjs-material-kit.scss?v=1.1.0";
-
-// * Page change when route change
-// Router.events.on("routeChangeStart", url => {
-//   console.log(`Loading: ${url}`);
-//   document.body.classList.add("body-page-transition");
-//   ReactDOM.render(
-//     <PageChange path={url} />,
-//     document.getElementById("page-transition")
-//   );
-// });
-// Router.events.on("routeChangeComplete", () => {
-//   ReactDOM.unmountComponentAtNode(document.getElementById("page-transition"));
-//   document.body.classList.remove("body-page-transition");
-// });
-// Router.events.on("routeChangeError", () => {
-//   ReactDOM.unmountComponentAtNode(document.getElementById("page-transition"));
-//   document.body.classList.remove("body-page-transition");
-// });
 
 export default class MyApp extends App {
   componentDidMount() {
