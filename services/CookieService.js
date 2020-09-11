508c80b5ab669d242a12255a42c4aa237d82752b
Muhammad Fakhri <muhammad_fakhri@apps.ipb.ac.id>
2020-09-11 14:13:50 +0700

    Add Cookie Service

---
+++ services/CookieService.js
@@ -0,0 +1,27 @@
+import Cookies from "js-cookie";
+// import { parseJwt } from "../utils/formatter";
+const KEY_TOKEN = "_z2lj4lY7Wn_hIz_";
+
+export default class CookieService {
+	static getToken() {
+		return Cookies.get(KEY_TOKEN);
+	}
+
+	static setToken(token = "") {
+		Cookies.set(KEY_TOKEN, token, { expires: 1 });
+	}
+
+	static removeToken() {
+		Cookies.remove(KEY_TOKEN);
+	}
+
+	// static checkToken() {
+	// 	const token = Cookies.get(KEY_TOKEN);
+	// 	return !!token;
+	// }
+
+	// static getUserFromToken() {
+	// 	const token = Cookies.get(KEY_TOKEN);
+	// 	return parseJwt(token);
+	// }
+}
